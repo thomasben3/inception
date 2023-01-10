ee5cb2af0d5b8142c86932dfc9d5868c023db514
Thomas Bensemhoun <tbensem@MacBook-Pro-de-Michael.local>
2023-01-10 16:51:32 +0100

    9

--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -6,3 +6,13 @@ if [! -e /var/www/wordpress/wp-config.php]; then
 	--dbuser=$SQL_USER \
 	--dbpass=$SQL_PASSWORD \
 	--dbhost=mariadb:3306 --path='/var/www/wordpress'
+
+sleep 10
+wp core install     --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root --path='/var/www/wordpress'
+wp user create      --allow-root --role=author $USER1_LOGIN $USER1_MAIL --user_pass=$USER1_PASS --path='/var/www/wordpress' >> /log.txt
+fi
+
+if [ ! -d /run/php ]; then
+    mkdir ./run/php
+fi
+/usr/sbin/php-fpm7.3 -F
