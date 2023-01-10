f5fc50dbfb5ccaebbb0203763c48d534996b6c26
Thomas Bensemhoun <tbensem@MacBook-Pro-de-Michael.local>
2023-01-10 14:16:21 +0100

    8

---
+++ srcs/requirements/wordpress/tools/config.sh
@@ -0,0 +1,8 @@
+sleep 10
+if [! -e /var/www/wordpress/wp-config.php]; then
+	wp config create \
+	--allow-root \
+	--dbname=$SQL_DATABASE \
+	--dbuser=$SQL_USER \
+	--dbpass=$SQL_PASSWORD \
+	--dbhost=mariadb:3306 --path='/var/www/wordpress'
